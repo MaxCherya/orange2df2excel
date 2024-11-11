07bd72d28b998c36db2544ae066698456b4578ac
Max <maxchergik@gmail.com>
2024-11-11 10:53:43 +0200

    hash_password implementation

--- orange2df2excel/__init__.py
+++ orange2df2excel/__init__.py
@@ -2,4 +2,4 @@ from .orange_tools import raw_data_to_excel
 from .orange_tools import fetch_kobo_data
 from .orange_tools import fetch_surveycto_data
 from .orange_tools import generate_bnf_id
-from .orange_tools import gen_encryption_key, encrypt_value, decrypt_value, rederive_key
+from .orange_tools import gen_encryption_key, encrypt_value, decrypt_value, rederive_key, hash_password
